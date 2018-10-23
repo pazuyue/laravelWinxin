09a9cb497a82d1d7d4e1b6d22e414b077ef775b7
yueguang <yueguang@qingmutec.com>
2018-10-23 15:02:06 +0800

    初始化

---
+++ public/getToken.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/23
+ * Time: 15:01
+ */
+
+define("TOKEN","yueguang");
+
+$signature = $_GET["signature"];
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];
+$echoStr = $_GET["echostr"];
+
+$tmpArr = array(TOKEN,$timestamp,$nonce);
+sort($tmpArr,SORT_STRING);
+$tmpStr = implode( $tmpArr );
+$tmpStr = sha1( $tmpStr );
+
+
+if( $signature == $tmpStr )
+{
+    echo $echoStr;
+}
+else
+    echo "Error";
