80122e8cc1401421c8a3937d80d5e6ca68038e05
yueguang <yueguang@qingmutec.com>
2018-10-23 17:30:02 +0800

    获取AccessToken，判断是否关注

--- app/Http/Controllers/WeiXin/WeiXinController.php
+++ app/Http/Controllers/WeiXin/WeiXinController.php
@@ -19,15 +19,13 @@ class WeiXinController extends Controller
         //设置抓取的url
         curl_setopt($curl, CURLOPT_URL, $url);
         //设置头文件的信息作为数据流输出
-        curl_setopt($curl, CURLOPT_HEADER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
         //设置获取的信息以文件流的形式返回，而不是直接输出。
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         //执行命令
         $data = curl_exec($curl);
         //关闭URL请求
         curl_close($curl);
-        dump($url);
-        dump($data);
         //显示获得的数据
        return $data;
     }
@@ -38,7 +36,7 @@ class WeiXinController extends Controller
         //设置抓取的url
         curl_setopt($curl, CURLOPT_URL, $url);
         //设置头文件的信息作为数据流输出
-        curl_setopt($curl, CURLOPT_HEADER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
         //设置获取的信息以文件流的形式返回，而不是直接输出。
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         //设置post方式提交
@@ -124,6 +122,7 @@ class WeiXinController extends Controller
     public function getAccessToken() {
          $url_get = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx98e276bea8ddeca5&secret=84415899ceb3c77b58364d2468023cf7';
          $json = $this->curlGet($url_get);
+        dump($json);
          if (!$json->errmsg) {
                 } else {
                     dump('获取access_token发生错误：错误代码'.$json->errcode.',微信返回错误信息：'.$json->errmsg) ;
