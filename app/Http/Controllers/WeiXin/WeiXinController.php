98735cf98b64055efc8bce344f908b59c1f6778f
yueguang <yueguang@qingmutec.com>
2018-10-23 17:58:29 +0800

    获取推送事件

--- app/Http/Controllers/WeiXin/WeiXinController.php
+++ app/Http/Controllers/WeiXin/WeiXinController.php
@@ -5,15 +5,16 @@
  * Date: 2018/10/23
  * Time: 16:04
  */
-namespace App\Http\Controllers\WeiXin;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
+namespace App\Http\Controllers\WeiXin;
+
+use App\Http\Controllers\Controller;
 
 class WeiXinController extends Controller
 {
 
-    private function curlGet($url){
+    private function curlGet($url)
+    {
         //初始化
         $curl = curl_init();
         //设置抓取的url
@@ -28,10 +29,11 @@ class WeiXinController extends Controller
         //关闭URL请求
         curl_close($curl);
         //显示获得的数据
-       return $data;
+        return $data;
     }
 
-    private function curlPost($url,$post_data){
+    private function curlPost($url, $post_data)
+    {
         //初始化
         $curl = curl_init();
         //设置抓取的url
@@ -43,10 +45,10 @@ class WeiXinController extends Controller
         //设置post方式提交
         curl_setopt($curl, CURLOPT_POST, 1);
         //设置post数据
-       /* $post_data = array(
-            "username" => "coder",
-            "password" => "12345"
-        );*/
+        /* $post_data = array(
+             "username" => "coder",
+             "password" => "12345"
+         );*/
         curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
         //执行命令
         $data = curl_exec($curl);
@@ -75,10 +77,11 @@ class WeiXinController extends Controller
     /**
      * 判断是否关注
      */
-    public function actionsubscribe(){
-        $token=$this->getAccessToken();
+    public function actionsubscribe()
+    {
+        $token = $this->getAccessToken();
 
-        $openid='oPuK51SdvJnSnBVoiaIPpce0ebvE';
+        $openid = 'oPuK51SdvJnSnBVoiaIPpce0ebvE';
         $subscribe_msg = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$token&openid=$openid";
         $subscribe = json_decode($this->curlGet($subscribe_msg));
         dump($subscribe);
@@ -97,40 +100,59 @@ class WeiXinController extends Controller
 
 
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_HEADER,0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         $data = curl_exec($ch);
-        $data = json_decode($data,true);
+        $data = json_decode($data, true);
         curl_close($ch);
         dump($data);
 
         $access_token = $data['access_token'];
         $openid = $data['openid'];
-        $get_user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $get_user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $access_token . '&openid=' . $openid . '&lang=zh_CN';
 
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL,$get_user_info_url);
-        curl_setopt($ch,CURLOPT_HEADER,0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($ch, CURLOPT_URL, $get_user_info_url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         $res = curl_exec($ch);
         curl_close($ch);
-        $res = json_decode($res,true);
+        $res = json_decode($res, true);
     }
 
-    public function getAccessToken() {
-         $url_get = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx98e276bea8ddeca5&secret=84415899ceb3c77b58364d2468023cf7';
-         $json = $this->curlGet($url_get);
-        dump($json->access_token);
-         if (isset($json->errmsg)) {
-             dump('获取access_token发生错误：错误代码'.$json->errcode.',微信返回错误信息：'.$json->errmsg) ;
-             return "";
-         }
-         return $json['access_token'];
+    public function getAccessToken()
+    {
+        $url_get = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx98e276bea8ddeca5&secret=84415899ceb3c77b58364d2468023cf7';
+        $json = $this->curlGet($url_get);
+        $json = json_decode($json);
+        if (isset($json->errmsg)) {
+            dump('获取access_token发生错误：错误代码' . $json->errcode . ',微信返回错误信息：' . $json->errmsg);
+            return "";
         }
+        return $json->access_token;
+    }
 
+    public function responseMsg(){
+        /*
+        获得请求时POST:XML字符串
+        不能用$_POST获取，因为没有key
+         */
+        $xml_str = $GLOBALS['HTTP_RAW_POST_DATA'];
+        if(empty($xml_str)){
+            die('');
+        }
+        if(!empty($xml_str)){
+            // 解析该xml字符串，利用simpleXML
+            libxml_disable_entity_loader(true);
+            //禁止xml实体解析，防止xml注入
+            $request_xml = simplexml_load_string($xml_str, 'SimpleXMLElement', LIBXML_NOCDATA);
+            //判断该消息的类型，通过元素MsgType
+            return $request_xml;
+        }
+    }
 
 
 }
