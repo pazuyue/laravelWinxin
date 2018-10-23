cc589d7632b36672455edbf34f5f3e605603b328
yueguang <yueguang@qingmutec.com>
2018-10-23 16:08:07 +0800

    微信获取opendID

---
+++ app/Http/Controllers/WinXin/WenXinController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/23
+ * Time: 16:04
+ */
+namespace App\Http\Controllers\WinXin;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class WenXinController extends BaseController
+{
+
+    public function getOpenid(Request $request){
+            dump(123);
+    }
+
+}
