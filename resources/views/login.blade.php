99796756d93e827749276ab7332134f30906c9eb
yueguang <yueguang@qingmutec.com>
2018-10-24 15:17:08 +0800

    登陆

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,12 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<script>
+    window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+</script>
 <head>用户登录</head>
 <form name="LoginForm" method="post" action="{{url('/login')}}">
     <p>
